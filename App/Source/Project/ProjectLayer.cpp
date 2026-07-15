9796d50a1f13b86721b22e9dc45689c9b004483f
Linh Trần <vanlinhtran50@gmail.com>
2026-07-15 15:50:09 +0700

    project code

--- App/Source/Project/ProjectLayer.cpp
+++ App/Source/Project/ProjectLayer.cpp
@@ -2,7 +2,6 @@
 #include "IconsFontAwesome6.h"
 
 #include "ProjectLayer.h"
-
 #include "Service/ProjectService.h"
 
 #include <cctype>
@@ -47,14 +46,14 @@ void ProjectLayer::OnRenderTopNavBarExtensions()
 	auto DrawTab = [&](const char* label, ProjectTab tabVal) {
 		ImGui::SetCursorPos(ImVec2(currentX, centerY));
 		bool isActive = (m_ActiveTab == tabVal);
-		
+
 		if (isActive)
 			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(0.0f, 173.0f / 255.0f, 181.0f / 255.0f, 1.0f)); // #00ADB5
 		else
 			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(187.0f / 255.0f, 201.0f / 255.0f, 202.0f / 255.0f, 1.0f)); // #BBC9CA
 
 		ImVec2 textSize = ImGui::CalcTextSize(label);
-		
+
 		// Invisible button for interaction
 		ImGui::SetCursorPos(ImVec2(currentX, 0));
 		if (ImGui::InvisibleButton(label, ImVec2(textSize.x, topbarHeight)))
@@ -71,14 +70,14 @@ void ProjectLayer::OnRenderTopNavBarExtensions()
 		// Draw active bottom border
 		if (isActive)
 		{
-			drawList->AddLine(ImVec2(pMin.x + currentX, pMin.y + topbarHeight - 2.0f), 
-							  ImVec2(pMin.x + currentX + textSize.x, pMin.y + topbarHeight - 2.0f), 
-							  IM_COL32(0, 173, 181, 255), 3.0f);
+			drawList->AddLine(ImVec2(pMin.x + currentX, pMin.y + topbarHeight - 2.0f),
+				ImVec2(pMin.x + currentX + textSize.x, pMin.y + topbarHeight - 2.0f),
+				IM_COL32(0, 173, 181, 255), 3.0f);
 		}
 
 		ImGui::PopStyleColor();
 		currentX += textSize.x + 30.0f; // Spacing between tabs
-	};
+		};
 
 	DrawTab("Tasks", ProjectTab::Tasks);
 	DrawTab("Milestones", ProjectTab::Milestones);
@@ -91,14 +90,35 @@ void ProjectLayer::OnRenderTopNavBarExtensions()
 	{
 		const char* inviteCode = m_Project.Code.c_str();
 		ImVec2 inviteSize = ImGui::CalcTextSize(inviteCode);
-		float inviteTotalWidth = inviteSize.x + 40.0f; // Padding + icon
-		float rightOffset = 220.0f; // Before the bell icon which is around 170px from right
+
+		float iconSize = ImGui::CalcTextSize(ICON_FA_COPY).x;
+		float inviteTotalWidth = inviteSize.x + iconSize + 30.0f; // Padding
+		float rightOffset = 220.0f;
 
 		ImVec2 badgePos = ImVec2(pMin.x + childSize.x - rightOffset - inviteTotalWidth, pMin.y + (topbarHeight - 24.0f) * 0.5f);
 
 		drawList->AddRectFilled(badgePos, ImVec2(badgePos.x + inviteTotalWidth, badgePos.y + 24.0f), IM_COL32(51, 53, 53, 255), 4.0f);
-		drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(), ImVec2(badgePos.x + 10.0f, badgePos.y + 4.0f), IM_COL32(187, 201, 202, 255), inviteCode);
-		drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(), ImVec2(badgePos.x + inviteSize.x + 20.0f, badgePos.y + 4.0f), IM_COL32(187, 201, 202, 255), ICON_FA_COPY);
+		drawList->AddText(ImGui::GetFont(), ImGui::GetFontSize(), ImVec2(badgePos.x + 10.0f, badgePos.y + (24.0f - inviteSize.y) * 0.5f), IM_COL32(187, 201, 202, 255), inviteCode);
+
+		ImGui::SetCursorScreenPos(ImVec2(badgePos.x + inviteSize.x + 15.0f, badgePos.y));
+
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(1.0f, 1.0f, 1.0f, 0.1f));
+		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(1.0f, 1.0f, 1.0f, 0.2f));
+		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(187.0f / 255.0f, 201.0f / 255.0f, 202.0f / 255.0f, 1.0f));
+
+		if (ImGui::Button(ICON_FA_COPY "##CopyCodeBtn", ImVec2(iconSize + 10.0f, 24.0f)))
+		{
+			ImGui::SetClipboardText(inviteCode);
+		}
+
+		if (ImGui::IsItemHovered())
+		{
+			ImGui::SetTooltip("Copy Project Code");
+			ImGui::SetMouseCursor(ImGuiMouseCursor_Hand);
+		}
+
+		ImGui::PopStyleColor(4);
 	}
 }
 
@@ -119,11 +139,12 @@ void ProjectLayer::OnRenderContent()
 	// Render the content for the currently active tab
 	switch (m_ActiveTab)
 	{
-		case ProjectTab::Tasks: m_TasksView.OnRender(projectName, createdDate); break;
-		case ProjectTab::Milestones:
-			m_MilestonesView.OnRender(projectName, createdDate, m_Project.Id, IsLeaderRole(m_Project.Role));
-			break;
-		case ProjectTab::Chart: m_ChartView.OnRender(projectName, createdDate); break;
-		case ProjectTab::Workload: m_WorkloadView.OnRender(projectName, createdDate); break;
-        case ProjectTab::Member: m_MemberView.OnRender(m_Project.Id, projectName, createdDate); break;}
+	case ProjectTab::Tasks: m_TasksView.OnRender(projectName, createdDate); break;
+	case ProjectTab::Milestones:
+		m_MilestonesView.OnRender(projectName, createdDate, m_Project.Id, IsLeaderRole(m_Project.Role));
+		break;
+	case ProjectTab::Chart: m_ChartView.OnRender(projectName, createdDate); break;
+	case ProjectTab::Workload: m_WorkloadView.OnRender(projectName, createdDate); break;
+	case ProjectTab::Member: m_MemberView.OnRender(m_Project.Id, projectName, createdDate); break;
+	}
 }
